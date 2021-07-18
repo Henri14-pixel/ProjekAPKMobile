6441f7c1f31e8e49f070e22363d32d90e05927f2
Vian Azis <59288339+VianTio@users.noreply.github.com>
2021-07-18 22:11:17 +0700

    Update Resource dan Tampilan Cart

--- KapronPetshop2/app/src/main/java/com/henribambangs/kapronpetshop/DashboardActivity.java
+++ KapronPetshop2/app/src/main/java/com/henribambangs/kapronpetshop/DashboardActivity.java
@@ -4,14 +4,19 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.henribambangs.kapronpetshop.Util.HttpsTrustManager;
+
 public class DashboardActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
     }
 
     public void onDetails(View view) {
